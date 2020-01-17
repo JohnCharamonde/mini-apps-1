2a0c0fab2353618ee4fdc4fe9220818f48dee53e
JohnCharamonde <john.charamonde@gmail.com>
2020-01-17 11:14:01 -0500

    Connect Four is working correctly.

--- challenge_4/client/src/components/Board.jsx
+++ challenge_4/client/src/components/Board.jsx
@@ -16,32 +16,68 @@ class Board extends React.Component {
   }
 
   handleClick(columnHead, e) {
-    var columnCopy = Array.from(this.state.columns)
+    var copyColumns = Array.from(this.state.columns);
     var targetId;
-    if(this.state.columns[columnHead][0] === 'RED' || this.state.columns[columnHead][0] === 'YELLOW') {
+
+    // check legal move - if not = notify ELSE copy columns and update to prepare for setState
+    if (this.state.columns[columnHead][0] === 'RED' || this.state.columns[columnHead][0] === 'YELLOW') {
       alert('This is an illegal move! Please try again!');
     } else {
-      for(let i = this.state.columns[columnHead].length - 1; i > -1; i--) {
-        if(typeof(this.state.columns[columnHead][i]) === 'number') {
+      for (let i = this.state.columns[columnHead].length - 1; i > -1; i--) {
+        if (typeof (this.state.columns[columnHead][i]) === 'number') {
           targetId = this.state.columns[columnHead][i];
-          columnCopy[columnHead][i] = this.state.turn;
+          copyColumns[columnHead][i] = this.state.turn;
+          console.log(targetId)
+          console.log(copyColumns)
           break;
         }
       }
+      //copy rows and update to prepare for setState
+      var copyRows = Array.from(this.state.rows);
+      for (let i = 0; i < this.state.rows.length; i++) {
+        for (let j = 0; j < this.state.rows[i].length; j++) {
+          if (this.state.rows[i][j] === targetId) {
+            copyRows[i][j] = this.state.turn;
+            console.log('copyRows', copyRows)
+          }
+        }
+      }
+
+      //copy checkForWinData and updata to prepare for setState
+      var copyCheckForWinData = Array.from(this.state.checkForWinData);
+      for (let i = 0; i < this.state.checkForWinData.length; i++) {
+        for (let j = 0; j < this.state.checkForWinData[i].length; j++) {
+          if (this.state.checkForWinData[i][j] === targetId) {
+            copyCheckForWinData[i][j] = this.state.turn;
+          }
+        }
+      }
+
+      // prepare to change state.turn
+      var newTurn = this.state.turn === 'RED' ? 'YELLOW' : 'RED';
+
+      // setState with updates
+      this.setState({
+        columns: copyColumns,
+        rows: copyRows,
+        checkForWinData: copyCheckForWinData,
+        turn: newTurn
+      })
+
     }
 
-
+    const areAllRed = currentVal => currentVal === 'RED';
+    const areAllYellow = currentVal => currentVal === 'YELLOW';
+    for(let i = 0; i < this.state.checkForWinData.length; i++) {
+      var redHasWon = this.state.checkForWinData[i].every(areAllRed);
+      var yellowHasWon = this.state.checkForWinData[i].every(areAllYellow);
+      if(redHasWon) {
+        alert('RED WINS!')
+      } else if (yellowHasWon) {
+        alert('YELLOW WINS!')
+      }
+    }
   }
-  // checkForWin
-  // checkIfMoveIsLegal
-  // tokenPlacementHandler
-
-
-  // resetBoardDatabaseToOriginal  -relevant to DB...
-  // checkForWin
-  // checkIfMoveIsLegal
-  // tokenPlacementHandler
-
   render() {
     let isRedTurn = this.state.turn === 'RED'
     return (
@@ -51,13 +87,13 @@ class Board extends React.Component {
           <thead>
             <tr>
               {this.state.columnHeads.map(columnHead =>
-                < ColumnHeadItem columnHead={columnHead} handleClick={this.handleClick.bind(this)}/>
+                < ColumnHeadItem columnHead={columnHead} handleClick={this.handleClick.bind(this)} />
               )}
             </tr>
           </thead>
           <tbody>
             {this.state.rows.map(row =>
-                < BoardRow row={row} />
+              < BoardRow row={row} />
             )}
           </tbody>
         </table>
